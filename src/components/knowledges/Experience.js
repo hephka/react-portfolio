fafe8728b2a151b4586f2d33ce1e16629328ac29
Hephka <franckthevenin95@gmail.com>
2021-02-13 10:07:27 +0100

    modèle

---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <h3>Expériences</h3>
+      <div className="exp1">
+        <h4>Directeur Technique - Apple</h4>
+        <h5>2018-2020</h5>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+          nihil qui eligendi ratione, quo cum quidem. Sed provident nemo beatae
+          accusantium, porro ab blanditiis ex magnam perferendis quaerat
+          mollitia sapiente obcaecati saepe molestiae totam fugiat cumque
+          ducimus quam.
+        </p>
+      </div>
+      <div className="exp2">
+        <h4>Directeur Technique - Apple</h4>
+        <h5>2018-2020</h5>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+          nihil qui eligendi ratione, quo cum quidem. Sed provident nemo beatae
+          accusantium, porro ab blanditiis ex magnam perferendis quaerat
+          mollitia sapiente obcaecati saepe molestiae totam fugiat cumque
+          ducimus quam.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
